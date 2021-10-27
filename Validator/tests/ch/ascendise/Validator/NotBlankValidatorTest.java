0fb16d60ccf24887d6f9f6c1e111b32fac078fc2
Ascendise <rapperido@gmail.com>
2021-10-27 13:07:42 +0200

    Test for NotBlank

---
+++ Validator/tests/ch/ascendise/Validator/NotBlankValidatorTest.java
@@ -0,0 +1,37 @@
+package ch.ascendise.Validator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import ch.ascendise.Validator.Annotations.*;
+
+class NotBlankValidatorTest {
+
+	@Test
+	void validFieldValue()
+	{
+		class Test
+		{
+			@NotBlank
+			private String notBlank = "aaaa";
+		}
+		var validator = new ValidatorImpl<Test>(new Test());
+		var isValid = validator.isValid();
+		assertTrue(isValid, "Correct field got detected as invalid");
+	}
+	
+	@Test
+	void invalidFieldValue()
+	{
+		class Test
+		{
+			@NotBlank
+			private String blank = " ";
+		}
+		var validator = new ValidatorImpl<Test>(new Test());
+		var isValid = validator.isValid();
+		assertFalse(isValid, "Invalid field passed");
+	}
+
+}
