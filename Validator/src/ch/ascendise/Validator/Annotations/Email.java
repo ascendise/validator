8219a7f4068065b0a0efdb02948478825b3688d2
ascendise <rapperido@gmail.com>
2021-11-27 14:41:35 +0100

    Added documentation for Email Annotation

--- Validator/src/ch/ascendise/Validator/Annotations/Email.java
+++ Validator/src/ch/ascendise/Validator/Annotations/Email.java
@@ -7,6 +7,14 @@ import java.lang.annotation.Target;
 
 import ch.ascendise.Validator.EmailValidator;
 
+/**
+ * Annotated field string value should be an email adress
+ * 
+ * This annotation does only check the pattern of the value
+ * If the email itself fits into the regex pattern but is not
+ * a registered email address, it will still pass
+ * 
+ */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 @ValidatorType(EmailValidator.class)
