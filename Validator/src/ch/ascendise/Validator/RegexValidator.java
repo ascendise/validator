29f8c50587a96caaa86b11d8fdddf3d841e974e6
ascendise <60201207+ascendise@users.noreply.github.com>
2022-02-07 00:13:21 +0100

    Improved RegexValidator docs

--- Validator/src/ch/ascendise/Validator/RegexValidator.java
+++ Validator/src/ch/ascendise/Validator/RegexValidator.java
@@ -4,11 +4,21 @@ import java.util.regex.Pattern;
 
 import ch.ascendise.Validator.Annotations.Regex;
 
+/**
+ * This is a validator for validating string fields with a pattern constraint.
+ * @see Regex
+ */
 public final class RegexValidator extends AbstractValidator 
 {
 	
 	private String pattern;
 
+	/**
+	 * Creates a new instance of the RegexValidator
+	 * @param object Value that gets validated.
+	 * @param fieldName Name of the field.
+	 * @param regex Annotation the field was annotated with.
+	 */
 	protected RegexValidator(Object object, String fieldName, Regex regex) 
 	{
 		super(object, fieldName, regex);
