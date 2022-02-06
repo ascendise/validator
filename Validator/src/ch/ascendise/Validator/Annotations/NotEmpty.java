40997d6259bbd887277538e5322d2f612fb08775
ascendise <60201207+ascendise@users.noreply.github.com>
2022-02-06 23:54:35 +0100

    Imrpoved NotEmpty-Annotation docs

--- Validator/src/ch/ascendise/Validator/Annotations/NotEmpty.java
+++ Validator/src/ch/ascendise/Validator/Annotations/NotEmpty.java
@@ -5,10 +5,10 @@ import ch.ascendise.Validator.NotEmptyValidator;
 
 /**
  * Annotated fields string value should not be empty.
- * 
+ * <br>
  * The length of the string value has to be more than 0
  * for the field to be considered valid
- * 
+ * <br>
  * The field should not be null
  */
 @Target(ElementType.FIELD)
