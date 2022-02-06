a8b2517fa73fc7bea7cb83cd26c0db76bf47f3c4
ascendise <60201207+ascendise@users.noreply.github.com>
2022-02-06 23:57:10 +0100

    Improved ValidatorType-Annotation docs

--- Validator/src/ch/ascendise/Validator/Annotations/ValidatorType.java
+++ Validator/src/ch/ascendise/Validator/Annotations/ValidatorType.java
@@ -5,7 +5,8 @@ import java.lang.annotation.*;
 import ch.ascendise.Validator.AbstractValidator;
 
 /**
- * Is used for annotating annotations used for setting constraints
+ * Is used to specify which validator is used when annotating
+ * a field with a Validator-Annotation
  */
 @Target(ElementType.ANNOTATION_TYPE)
 @Retention(RetentionPolicy.RUNTIME)
