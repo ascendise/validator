7bb093f1e873c297866f1c5db35e5b056eb0500c
ascendise <60201207+ascendise@users.noreply.github.com>
2022-02-06 23:50:17 +0100

    Improved Email-Annotation docs

--- Validator/src/ch/ascendise/Validator/Annotations/Email.java
+++ Validator/src/ch/ascendise/Validator/Annotations/Email.java
@@ -8,11 +8,18 @@ import java.lang.annotation.Target;
 import ch.ascendise.Validator.EmailValidator;
 
 /**
- * Annotated field string value should be an email adress
- * 
- * This annotation does only check the pattern of the value
+ * Annotated field string value should be an email adress.
+ * <br>
+ * This annotation does only check the pattern of the value.
  * If the email itself fits into the regex pattern but is not
- * a registered email address, it will still pass
+ * a registered email address, it will still pass.
+ * <br>
+ * Following email-patterns are accepted:
+ * <ul>
+ *  <li>user@domain.com</li>
+ *  <li>user@[10.9.8.7]</li>
+ *  <li>user@localhost.home</li>
+ * </ul>
  * 
  */
 @Target(ElementType.FIELD)
