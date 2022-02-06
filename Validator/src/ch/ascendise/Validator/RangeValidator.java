31caa80bfc48a30485146895d0a6138fbc73c781
ascendise <60201207+ascendise@users.noreply.github.com>
2022-02-07 00:11:04 +0100

    Improved RangeValidator docs

--- Validator/src/ch/ascendise/Validator/RangeValidator.java
+++ Validator/src/ch/ascendise/Validator/RangeValidator.java
@@ -2,11 +2,21 @@ package ch.ascendise.Validator;
 
 import ch.ascendise.Validator.Annotations.Range;
 
+/**
+ * This is a validator for validating numerical fields with a Range-Constraint
+ * @see Range 
+ */
 public final class RangeValidator extends AbstractValidator{
 	
 	private double min;
 	private double max;
 
+	/**
+	 * Creates a new instance of the RangeValidator
+	 * @param object Value that gets validated.
+	 * @param fieldName Name of the field.
+	 * @param range Annotation the field was annotated with.
+	 */
 	protected RangeValidator(Object object, String fieldName, Range range) {
 		super(object, fieldName, range);
 		this.min = range.min();
