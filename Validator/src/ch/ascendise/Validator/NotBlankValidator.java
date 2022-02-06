844d416ba5bb29d0581336315687f8f75c0874a3
ascendise <60201207+ascendise@users.noreply.github.com>
2022-02-07 00:03:55 +0100

    Improved NotBlankValidator docs

--- Validator/src/ch/ascendise/Validator/NotBlankValidator.java
+++ Validator/src/ch/ascendise/Validator/NotBlankValidator.java
@@ -2,9 +2,18 @@ package ch.ascendise.Validator;
 
 import ch.ascendise.Validator.Annotations.NotBlank;
 
+/**
+ * This is a validator for validating string fields that should not be blank.
+ * @see ch.ascendise.Validator.Annotations.NotBlank
+ */
 public final class NotBlankValidator extends AbstractValidator
 {
-	
+	/**
+	 * Creates a new instance of the NotBlankValidator.
+	 * @param object Value that gets validated
+	 * @param fieldName Name of the field.
+	 * @param notBlank Annotation the field was annotated with.
+	 */
 	protected NotBlankValidator(Object object, String fieldName, NotBlank notBlank)
 	{
 		super(object, fieldName, notBlank);
